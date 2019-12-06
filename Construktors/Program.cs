1d43ab5140d8cf39e92c7906294fa4a6a789219e
PW <przemek.wrobel@poczta.fm>
2019-12-06 13:54:17 +0100

    przeciążanie konstruktorów

--- Construktors/Program.cs
+++ Construktors/Program.cs
@@ -6,7 +6,12 @@ namespace Construktors
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // konstruktory przeciążone ilością parametrów przekazanych w konstruktorze
+            Person person = new Person();
+            Person person2 = new Person(1, "Tomek");
+            Person person3 = new Person(2, "BlaBla", "OLO");
+            Person person4 = new Person(3, "OLA", "Tola", "Warszawa", 16, "women");
+
         }
     }
 }
