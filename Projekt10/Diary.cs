7916fd7be18afd202a99b1800399b1fdd3a96d27
PW <przemek.wrobel@poczta.fm>
2019-12-06 14:59:22 +0100

    klasa jako zmienna

---
+++ Projekt10/Diary.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Projekt10
+{
+    class Diary
+    {
+        /// <summary>
+        /// konstruktor ma taką samą nazwę jak nazwa klasy, może być ich wiele ale muszą się różnić przekazanymi argumentami lub typami
+        /// </summary>
+        public Diary()
+        {
+            ratings = new List<float>();
+        }
+        //stan (zmienne - pola)
+        List<float> ratings;
+
+        //zachowania
+
+        /// <summary>
+        /// Dodaje ocenę rating do listy ocen ratings
+        /// </summary>
+        /// <param name="rating"></param>
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating);
+        }
+
+        /// <summary>
+        /// Oblicza średnią z ocen z listy
+        /// </summary>
+        /// <returns></returns>
+        public float CalculateAverage()
+        {
+            float sum = 0;
+            float avg = 0;
+            foreach (var rating in ratings)
+            {
+                sum += rating;
+            }
+
+            avg = sum / ratings.Count();
+            return avg;
+        }
+
+        /// <summary>
+        /// Metoda zwraca najwyższą ocenę z listy
+        /// </summary>
+        /// <returns></returns>
+        public float GiveMaxRating()
+        {
+
+            return ratings.Max();
+        }
+
+        /// <summary>
+        /// Metoda zwraca najniższą ocenę z listy
+        /// </summary>
+        /// <returns></returns>
+        public float GiveMinRatin()
+        {
+            return ratings.Min();
+        }
+    }
+}
--- Projekt10/Program.cs
+++ Projekt10/Program.cs
@@ -6,7 +6,13 @@ namespace Projekt10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Diary diary = new Diary();
+            diary.AddRating(3);
+            diary.AddRating(5);
+
+
+            diary = new Diary();
+            diary.AddRating(8);
         }
     }
 }
