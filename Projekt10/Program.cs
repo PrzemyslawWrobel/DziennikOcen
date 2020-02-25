03088b20d03d29180b13e68a2923acb840d17abb
PW <przemek.wrobel@poczta.fm>
2020-02-25 13:13:21 +0100

    typy wartościowe

--- Projekt10/Program.cs
+++ Projekt10/Program.cs
@@ -4,15 +4,36 @@ namespace Projekt10
 {
     class Program
     {
+        static void GiveName(Diary diary)
+        {
+            diary.Name = "dziennik Babuni"; 
+        }
+
+        static void IncrementNumber(int number)
+        {
+            number += 1;
+        }
         static void Main(string[] args)
         {
+            Diary d1 = new Diary();
+            Diary d2 = d1;
 
+            GiveName(d1);
+            // d1 = new Diary();
+            //d1.Name = "Dziennik Marcina";
+            Console.WriteLine(d2.Name);
 
-            Diary diary = new Diary();
-            diary.AddRating(3);
-            diary.AddRating(5);
-            diary.AddRating(3.3f);
-            diary.AddRating(5.9f);
+            int x1 = 4;
+            //int x2 = x1;
+            IncrementNumber(x1);
+            x1 = 100;
+            Console.WriteLine(x1);
+
+            //Diary diary = new Diary();
+            //diary.AddRating(3);
+            //diary.AddRating(5);
+            //diary.AddRating(3.3f);
+            //diary.AddRating(5.9f);
 
             //Console.WriteLine(Diary.MaxGrade);
             //Diary diary1 = new Diary();
@@ -29,16 +50,8 @@ namespace Projekt10
             //Console.WriteLine("średnia ocena: " + stats.AverageGrade);
             //Console.WriteLine("MAX ocena: "  + stats.MaxGrade);
             //Console.WriteLine("Min ocena: " + stats.MinGrade);
-            Diary d1 = new Diary();
-            Diary d2 = d1;
-             d1 = new Diary();
-            d1.Name = "Dziennik Marcina";
-            Console.WriteLine(d2.Name);
 
-            int x1 = 4;
-            int x2 = x1;
-            x1 = 100;
-            Console.WriteLine(x2);
+
 
 
         }
